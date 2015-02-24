62f19ec935975194c843bdcbb1ef874dadf20601
mengzhou <mengzhou@usc.edu>
2015-02-23 18:32:15 -0800

    Partial commit.

--- mapping.py
+++ mapping.py
@@ -18,76 +18,70 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys, os, re
-import logging, subprocess
-from optparse import OptionParser
-from random import choice
+import sys, logging, subprocess
 
 class Mapping:
   """The main class for read mapping.
   """
   def __init__(self, opt, files):
-    self.files = files
-    self.file_list = self.files.get_file_list()
+    # things passed from options
     self.info = opt.info
     self.warn = opt.warn
     self.error = opt.error
     self.mapper = opt.mapper
+    self.directional = opt.directional
     self.isPairEnd = opt.isPairEnd
-    self.__get_file_paths(opt)
-    self.__set_default_par()
+    # get all file names
+    self.files = files
+    self.file_list = self.files.get_file_list()
+    self.index = opt.index
+    self._indexCT = self.index + "/CTgenome"
+    self._indexGA = self.index + "/GAgenome"
 
-  def map(self):
-    """Start mapping.
-    """
-    #self.info("Mapping reads to C to T converted genome...")
-    #self.info("Command: %s"%" ".join(self.args_map_to_CT))
-    self.__map_to_CT()
+  def map_se(self):
+    if self.directional:
+      total = 4
+    else:
+      total = 2
+    self.info("(1/%d) Mapping C to T converted read " + \
+      "to C to T converted genome..."%total)
+    self._call_mapper(self.file_list["read1CT"], self._indexCT)
+    self.info("(2/%d) Mapping C to T converted read " + \
+      "to G to A converted genome...")
+    self._call_mapper(self.file_list["read1CT"], self._indexGA)
+    if self.directional:
+      self.info("(3/%d) Mapping G to A converted read " + \
+        "to C to T converted genome..."%total)
+      self._call_mapper(self.file_list["read1CT"], self._indexCT)
+      self.info("(4/%d) Mapping G to A converted read " + \
+        "to G to A converted genome...")
+      self._call_mapper(self.file_list["read1CT"], self._indexGA)
 
-    #self.info("Mapping reads to G to A converted genome...")
-    #self.info("Command: %s"%" ".join(self.args_map_to_GA))
-    self.__map_to_GA()
+  def _call_mapper(self, read, index, output):
+    cmd = self._set_mapping_args(read, index)
+    try:
+      subprocess.check_call(cmd)
+    except subprocess.CalledProcessError:
+      self.error("An error occured for command:\n%s"%cmd)
+      sys.exit(1)
 
-  def __map_to_CT(self):
-    print "mapping to CT"
-
-  def __map_to_GA(self):
-    print "mapping to GA"
-
-  def __get_file_paths(self, opt):
-    self.mate1 = opt.mate1
-    self.mate1_CT = opt.mate1_CT
-    self.mate1_GA = opt.mate1_GA
-    if opt.isPairEnd:
-      self.mate2 = opt.mate2
-      self.mate2_CT = opt.mate2_CT
-      self.mate2_GA = opt.mate2_GA
-
-  def __set_default_par(self):
-    self.par = {}
+  def _set_mapping_args(self, read, index):
+    return []
 
 class MappingWithTophat2(Mapping):
-  def __set_default_par(self):
-    self.par = {\
-      "--readFilesIn":$READ, \
-      "--outFileNamePrefix":$OUTPUT, \
-      "--outFilterIntronMotifs":"RemoveNoncanonical", \
-      "--outSAMstrandField":"intronMotif", \
-      "--clip3pAdapterSeq":"", \
-      "--outSAMtype":"BAM Unsorted" \
+  def _set_mapping_args(self, read, index, output):
+    args = {\
     }
 
 class MappingWithSTAR(Mapping):
-  def __set_default_par(self):
-    if self.isPairEnd:
-      reads = " ".join(self.files.get_mate1(), self.files.get_mate2())
-    else:
-      reads = self.files.get_mate1()
-    self.par = {\
+  def _set_mapping_args(self, read, index, output):
+    args = {\
+      "--runThreadN":"8", \
       "--readFilesIn":reads, \
-      "--outFileNamePrefix":$OUTPUT, \
+      "--genomeDir":index, \
+      "--outFileNamePrefix":output, \
       "--outFilterIntronMotifs":"RemoveNoncanonical", \
+      #"--clip3pAdapterSeq":"", \
       "--outSAMstrandField":"intronMotif", \
-      "--clip3pAdapterSeq":"", \
       "--outSAMtype":"BAM Unsorted" \
     }
